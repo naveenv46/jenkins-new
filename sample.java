757e39385d0c9ed83d794c3420499396b828c25a
naveenv46 <46064659+naveenv46@users.noreply.github.com>
2019-02-17 16:37:07 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -178,8 +178,7 @@ public class AssistiveExample extends JPanel
     //if (context.getAccessibleEditableText() != null)
     //  editableTextCheckBox.setSelected(true);
     repaint();
-  }
-}
+  
 
 
 
